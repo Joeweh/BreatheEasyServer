373271d82e60e20d370988fe4c25c349018077ac
Joeweh <joeyqsa@outlook.com>
2024-05-16 18:13:42 -0400

    Added functionality to get autocomplete options based on search query and location

---
+++ bin/config.dart
@@ -0,0 +1,5 @@
+import 'package:dotenv/dotenv.dart';
+
+class Env {
+  static late final DotEnv vars;
+}
--- bin/server.dart
+++ bin/server.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:dotenv/dotenv.dart';
@@ -5,8 +6,11 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
+import 'config.dart';
+import 'utils/places.dart';
+
 void main(List<String> args) async {
-  DotEnv(includePlatformEnvironment: true).load();
+  Env.vars = DotEnv(includePlatformEnvironment: false)..load();
 
   final app = Router();
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
@@ -14,10 +18,33 @@ void main(List<String> args) async {
       .addMiddleware(logRequests())
       .addHandler(app.call);
 
-  //app.mount("/api/users", UserAPI().router);
-
-  app.get("/api", (Request request) {
+  app.get('/api', (Request request) {
     return Response(HttpStatus.ok, body: "SERVER ONLINE");
+  });
+
+  app.post('/api/places', (Request request) async {
+    final data = jsonDecode(await request.readAsString());
+
+    final query = data['query'];
+    final location = data['location'];
+
+    var coordinates = LatLong(latitude: location['lat'], longitude: location['long']);
+
+    var response = await Places.getAutocomplete(query, coordinates);
+
+    var body = jsonDecode(response.body);
+
+    var suggestions = body['suggestions'];
+
+    var places = <PlacePrediction>[];
+
+    for (var placePredictionMap in suggestions) {
+      var placePrediction = PlacePrediction.fromJson(placePredictionMap);
+
+      places.add(placePrediction);
+    }
+
+    return Response(HttpStatus.ok, body: jsonEncode(places));
   });
 
   final server = await serve(handler, '0.0.0.0', port);
